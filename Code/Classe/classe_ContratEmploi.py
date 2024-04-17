09b2e9c7a67d4e582d733524f88749bef16bccd6
Your Name <2256853@cegepoutaouais.qc.ca>
2024-04-17 16:52:47 -0400

    fin de période

--- Code/Classe/classe_ContratEmploi.py
+++ Code/Classe/classe_ContratEmploi.py
@@ -1,4 +1,4 @@
-class ContrateEmploi:
+class ContratEmploi:
     """
     Classe Equipe
     """
@@ -24,12 +24,6 @@ class ContrateEmploi:
 
     def set_nb_heures_semaine(self, nb_heures_semaine):
         self.nb_heures_semaine =
-
-    def get_identifiant_contrat(self):
-        return self
-
-    def set_identifiant_contrat(self, identifiant_contrat):
-        self.identifiant_contrat = identifiant_contrat
 
     def get_salaire_de_base(self):
         return self.salaire_de_base
