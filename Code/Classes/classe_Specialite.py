e711fc33433e40eaeff9c68320de7b75b551ed0e
unknown <2360530@cegepoutaouais.qc.ca>
2024-04-18 11:02:36 -0400

    deuxième passage classe Spécialité

--- Code/Classes/classe_Specialite.py
+++ Code/Classes/classe_Specialite.py
@@ -5,7 +5,8 @@ class Specialite:
 
     # Création de la list_specialite
     list_specialite = []
-    def __init__(self, p_nom:str = "", p_description:str = ""):
+
+    def __init__(self, p_nom: str = "", p_description: str = ""):
         """
         Constructeur de la classe Specialite
         :param p_nom: Nom de la spécialité
@@ -42,7 +43,7 @@ class Specialite:
     def ajouter_specialite(cls, nouvelle_specialite: str):
         """
         Ajouter une spécialité dans list_specialite
-        :param nouvelle_specialite: Nom de la spécialité
+        :param nouvelle_specialite: Nom de la spécialité à ajouter
         """
         for specialite in cls.list_specialite:
             if specialite.nom == nouvelle_specialite:
@@ -51,25 +52,31 @@ class Specialite:
 
     # Modifier une spécialité
     @classmethod
-    def modifier_specialite(cls, specialite_a_modifier: str, nouvelle_specialite: str):
+    def modifier_specialite(cls, specialite_a_modifier: str, nouvelle_specialite: str, nouvelle_description: str):
         """
-        Modifie le nom d'une spécialité
+        Modifie le nom et la description d'une spécialité.
         :param specialite_a_modifier: Le nom actuel de la spécialité
         :param nouvelle_specialite: Le nouveau nom de la spécialité
+        :param nouvelle_description: La nouvelle description de la spécialité
         """
         for specialite in cls.list_specialite:
             if specialite.nom == specialite_a_modifier:
                 specialite.nom = nouvelle_specialite
+                specialite.description = nouvelle_description
 
     @staticmethod
-    def trouve_specialite(specialite_a_trouver: str) -> list:
+    def trouve_specialite(specialite_demandee: str) -> list:
         """
-        Une méthode statique qui permet de trouver tout les employés qui ont selon une spécialité.
-        :param specialite_a_trouver: Nom de la spécialitée à trouver
+        Une méthode statique qui permet de trouver tout les employés d'une même spécialité.
+        :param specialite_demandee: Nom de la spécialitée demandée.
+        :return: La liste des employés de la spécialité demandée.
         """
-        list_specialite_demmande = []
+        list_employes_selon_specialite = []
+
         for specialite in Specialite.list_specialite:
-            if specialite_a_trouver == specialite.nom:
-                list_specialite_demmande.append(specialite)
-        return list_specialite_demmande
+            if specialite_demandee == specialite.nom:
+                list_employes_selon_specialite.append(specialite)
+
+        if len(list_employes_selon_specialite) > 0:
+            return list_employes_selon_specialite
 
