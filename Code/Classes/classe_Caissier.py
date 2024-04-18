211ccb2e17436a8092e81932535a080cb0a878be
benjamin <2371875@cegepoutaouais.qc.ca>
2024-04-18 11:21:18 -0400

    héritage commmis et caissier

--- Code/Classes/classe_Caissier.py
+++ Code/Classes/classe_Caissier.py
@@ -1,18 +1,11 @@
-# Importation de la classe Gestionnaire
-<<<<<<< HEAD
-from classe_Gestionnaire import Gestionnaire
-# Importation de la class Specialite
-from classe_Specialite import Specialite
-=======
-
 from classe_Gestionnaire import Gestionnaire
 from classe_Specialite import Specialite
+from classe_ContratEmploi import ContratEmploi
+from datetime import date
+from classe_Employe import Employe
 
 
->>>>>>> 3f52adaadf531f7280a1369d1799e416be02ad5e
-
-
-class Caissier:
+class Caissier(Employe):
     """
     Classe Caissier enfant de la classe mère Employe.
     """
@@ -20,11 +13,14 @@ class Caissier:
     # Création de la liste_caissier.
     liste_caissier = []
 
-    def __init__(self, p_gestionnaire: Gestionnaire = None, p_specialite: Specialite = None):
+    def __init__(self, p_gestionnaire: Gestionnaire = None, p_specialite: Specialite = None, p_identifiant: str = "",
+                 p_nom: str = "", p_prenom: str = "", p_poste: any = "", p_date_engagement: date = None,
+                 p_contrat: ContratEmploi = None):
         """
         Constructeur de la classe Caissier
         :param p_gestionnaire: Le gestionnaire du caissier.
         :param p_specialite: La spécialité du caissier
         """
+        Employe.__init__(self, p_identifiant, p_nom, p_prenom, p_poste, p_date_engagement, p_contrat)
         self.gestionnaire = p_gestionnaire
         self.specialite = p_specialite
--- Code/Classes/classe_Commis.py
+++ Code/Classes/classe_Commis.py
@@ -2,6 +2,9 @@
 from Code.Classes.classe_Gestionnaire import Gestionnaire
 # Importation de la class Specialite
 from Code.Classes.classe_Specialite import Specialite
+from datetime import date
+from classe_Employe import Employe
+from classe_ContratEmploi import ContratEmploi
 
 
 class Commis:
@@ -12,11 +15,14 @@ class Commis:
     # Création de list_commis
     list_commis = []
 
-    def __init__(self, p_gestionnaire: Gestionnaire = None, p_specialite: Specialite = None):
+    def __init__(self, p_gestionnaire: Gestionnaire = None, p_specialite: Specialite = None, p_identifiant: str = "",
+                 p_nom: str = "", p_prenom: str = "", p_poste: any = "", p_date_engagement: date = None,
+                 p_contrat: ContratEmploi = None):
         """
         Constructeur de la classe Commis
         :param p_gestionnaire: Le gestionnaire du commis
         :param p_specialite: La spécialité du commis
         """
+        Employe.__init__(self, p_identifiant, p_nom, p_prenom, p_poste, p_date_engagement, p_contrat)
         self.gestionnaire = p_gestionnaire
         self.specialite = p_specialite
