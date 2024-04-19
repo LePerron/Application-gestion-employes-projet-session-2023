8ce6cad1274f0de4ae18f479dd763ecc731421fa
Marc-Antoine <2360530@cegepoutaouais.qc.ca>
2024-04-19 11:22:14 -0400

    7ÈME PASSAGE Classe GESTIONNAIRE terminé

--- Code/Classes/classe_Gestionnaire.py
+++ Code/Classes/classe_Gestionnaire.py
@@ -28,47 +28,47 @@ class Gestionnaire(Employe):
         self.liste_commis = p_liste_commis
         self.liste_caissier = p_liste_caissier
 
-    def ajouter_commis_a_liste(self, nom_commis_a_ajouter: str) -> None:
+    def ajouter_commis_a_liste(self, identifiant_commis_a_ajouter: str) -> None:
         """
         Ajouter un commis dans liste_commis que le gérant gère.
-        :param nom_commis_a_ajouter: Commis à ajouter
-        :return: La liste des commis avec le commis ajouté
+        :param identifiant_commis_a_ajouter: L'identifiant du commis à ajouter
         """
         for commis in self.liste_commis:
-            if commis.nom == nom_commis_a_ajouter:
-                return
-        self.liste_commis.append(nom_commis_a_ajouter)
+            if commis.identifiant != identifiant_commis_a_ajouter:
+                continue
+            else:
+                if commis not in self.liste_commis:
+                    self.liste_commis.append(commis)
 
-    def supprimer_commis_a_liste(self, nom_commis_a_supprimer: str) -> None:
+    def supprimer_commis_a_liste(self, identifiant_commis_a_supprimer: str) -> None:
         """
         Supprimer un commis dans liste_commis que le gérant gère.
-        :param nom_commis_a_supprimer: Commis à supprimer
-        :return: La liste des commis avec le commis supprimé
+        :param identifiant_commis_a_supprimer: L'identifiant du commis à supprimer
         """
         for commis in self.liste_commis:
-            if commis.nom == nom_commis_a_supprimer:
-                self.liste_commis.remove(nom_commis_a_supprimer)
+            if commis.identifiant == identifiant_commis_a_supprimer:
+                self.liste_commis.remove(commis)
 
-    def ajouter_caissier_a_liste(self, nom_caissier_a_ajouter: str) -> None:
+    def ajouter_caissier_a_liste(self, identifiant_caissier_a_ajouter: str) -> None:
         """
         Ajouter un caissier dans liste_caissier que le gérant gère.
-        :param nom_caissier_a_ajouter: Caissier à ajouter
-        :return: La liste des caissier avec le caissier ajouté
+        :param identifiant_caissier_a_ajouter: L'identifiantCaissier du caissier à ajouter
         """
         for caissier in self.liste_caissier:
-            if caissier.nom == nom_caissier_a_ajouter:
-                return
-        self.liste_caissier.append(nom_caissier_a_ajouter)
+            if caissier.identifiant != identifiant_caissier_a_ajouter:
+                continue
+            else:
+                if caissier not in self.liste_caissier:
+                    self.liste_caissier.append(caissier)
 
-    def supprimer_caissier_a_liste(self, nom_caissier_a_supprimer: str) -> None:
+    def supprimer_caissier_a_liste(self, identifiant_caissier_a_supprimer: str) -> None:
         """
         Supprimer un caissier dans liste_caissier que le gérant gère.
-        :param nom_caissier_a_supprimer: Caissier à supprimer
-        :return: La liste des caissier avec le caissier supprimé
+        :param identifiant_caissier_a_supprimer: L'identifiant du caissier à supprimer
         """
         for caissier in self.liste_caissier:
-            if caissier.nom == nom_caissier_a_supprimer:
-                self.liste_caissier.remove(nom_caissier_a_supprimer)
+            if caissier.identifiant == identifiant_caissier_a_supprimer:
+                self.liste_caissier.remove(identifiant_caissier_a_supprimer)
 
     @staticmethod
     def parcourir_liste(liste_a_parcourir: list) -> str:
