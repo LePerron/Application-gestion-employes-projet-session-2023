9efcf83f684bb048c5a1140d7e0847928fa70ae5
benjamin <2371875@cegepoutaouais.qc.ca>
2024-04-19 08:13:30 -0400

    modification des privation et début du uml

--- Code/Classes/classe_ContratEmploi.py
+++ Code/Classes/classe_ContratEmploi.py
@@ -26,11 +26,16 @@ class ContratEmploi:
         self._termes_embauche = p_termes_embauche
         self._employe = p_employe
 
+        if len(self.list_contrat) < 1:
+            self.identifiant_contrat = 1
+        else:
+            self.identifiant_contrat = len(ContratEmploi.list_contrat) + 1
+
     def _get_nb_heures_semaine(self):
         return self._nb_heures_semaine
 
     def _set_nb_heures_semaine(self, v_nb_heures_semaine):
-        if isinstance(v_nb_heures_semaine, int) and v_nb_heures_semaine > 0:
+        if isinstance(v_nb_heures_semaine, int) and 40 > v_nb_heures_semaine > 0:
             self._nb_heures_semaine = v_nb_heures_semaine
 
     nb_heures_semaine = property(_get_nb_heures_semaine, _set_nb_heures_semaine)
@@ -39,7 +44,7 @@ class ContratEmploi:
         return self._salaire_de_base
 
     def _set_salaire_de_base(self, v_salaire_de_base):
-        if isinstance(v_salaire_de_base, float):
+        if isinstance(v_salaire_de_base, float) and v_salaire_de_base > 0:
             self._salaire_de_base = v_salaire_de_base
 
     salaire_de_base = property(_get_salaire_de_base, _set_salaire_de_base)
@@ -57,7 +62,7 @@ class ContratEmploi:
         return self._facteur_salaire
 
     def _set_facteur_salaire(self, v_facteur_salaire):
-        if isinstance(v_facteur_salaire, float):
+        if isinstance(v_facteur_salaire, float) and 100 > v_facteur_salaire > 0:
             self._facteur_salaire = v_facteur_salaire
 
     facteur_salaire = property(_get_facteur_salaire, _set_facteur_salaire)
@@ -71,6 +76,9 @@ class ContratEmploi:
     employe = property(_get_employe, _set_employe)
 
     def __str__(self):
-        return (f"IDENTIFIANT DU CONTRAT : {self.identifiant_contrat} - NOMBRE D'HEURE PAR SEMAINE : {self.nb_heures_semaine}"
-                f" - SALAIRE DE BASE : {self.salaire_de_base} - TERMES D'EMBAUCHE : {self.termes_embauche} "
-                f" - FACTEUR DU SALAIRE : {self.facteur_salaire} - NOM DE L'EMPLOYÉ{self.employe.nom}")
+        return (f"- IDENTIFIANT DU CONTRAT : {self.identifiant_contrat} - SALAIRE DE BASE : {self.salaire_de_base} $"
+                f"- TERMES D'EMBAUCHE : {self.termes_embauche} - NOMBRE D'HEURE PAR SEMAINE : {self.nb_heures_semaine} h"
+                f"- FACTEUR DU SALAIRE : {self.facteur_salaire} % - NOM DE L'EMPLOYÉ : {self.employe.nom}")
+
+
+
