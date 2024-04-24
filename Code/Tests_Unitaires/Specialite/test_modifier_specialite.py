e336ee9b57dc5380a622bc4d0e103345d67f6e8c
marc-Antoine <2360530@cegepoutaouais.qc.ca>
2024-04-24 17:15:34 -0400

    test_unitaires

--- Code/Tests_Unitaires/Specialite/test_modifier_specialite.py
+++ Code/Tests_Unitaires/Specialite/test_modifier_specialite.py
@@ -1,25 +1,19 @@
 from Projet_intra_Entreprise.Code.Classes.classe_Specialite import Specialite
 import pytest
 
-specialite1 = Specialite("Viandes", "ancienne description de Viande")
-specialite2 = Specialite("Legumes", "ancienne description de legume")
-specialite3 = Specialite("Fruits", "ancienne description de fruits")
-
-liste_initiale = [Specialite("Legumes", "celui qui gère les légumes"),
-                   Specialite("Legume", "ancienne description de legume"),
-                   Specialite("Fruits", "ancienne description de fruits")]
-
-
+liste_initiale = [Specialite("Viandes", "ancienne description de Viande"),
+                  Specialite("Legumes", "ancienne description de legume"),
+                  Specialite("Fruits", "ancienne description de fruits")]
 
 liste_attendu_1 = [Specialite("Legumes", "celui qui gère les légumes"),
-                   Specialite("Legume", "ancienne description de legume"),
+                   Specialite("Legumes", "ancienne description de legume"),
                    Specialite("Fruits", "ancienne description de fruits")]
 
 liste_attendu_2 = [Specialite("Viandes", "ancienne description de Viande"),
-                   Specialite("Legume", "celui qui gère les Fruits"),
+                   Specialite("Legumes", "celui qui gère les Fruits"),
                    Specialite("Fruits", "celui qui gère les legumes")]
 
-liste_attendu_3 = [Specialite("Viande", "celui qui gère les légumes"),
+liste_attendu_3 = [Specialite("Viandes", "celui qui gère les légumes"),
                    Specialite("Fruits", "celui qui gère les Fruits"),
                    Specialite("Fruits", "ancienne description de fruits")]
 
@@ -37,5 +31,7 @@ def test_modifier_specialite(specialite_a_modifier, nouvelle_specialite, nouvell
     :param nouvelle_description: La nouvelle description de la spécialité
     :param resultat_attendu: Le résultat attendu
     """
+    Specialite.list_des_specialites = liste_initiale
     Specialite.modifier_specialite(specialite_a_modifier, nouvelle_specialite, nouvelle_description)
-    assert Specialite.list_des_specialites == resultat_attendu
+    for index, specialite in enumerate(Specialite.list_des_specialites):
+        assert specialite.nom == resultat_attendu[index].nom
