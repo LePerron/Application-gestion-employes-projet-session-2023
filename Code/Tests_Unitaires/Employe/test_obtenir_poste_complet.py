97a2efc7d3105d7d7fd0e8913cbe65c07f249523
marc-Antoine <2360530@cegepoutaouais.qc.ca>
2024-04-24 14:57:30 -0400

    correction de choses qui marche pas

--- Code/Tests_Unitaires/Employe/test_obtenir_poste_complet.py
+++ Code/Tests_Unitaires/Employe/test_obtenir_poste_complet.py
@@ -1,12 +1,16 @@
-from Projet_intra_Entreprise.Code.Classes.classe_Employe import Employe
 from Projet_intra_Entreprise.Code.Classes.classe_Caissier import Caissier
-from Projet_intra_Entreprise.Code.Classes.classe_Specialite import Specialite
+
 import pytest
+
+from Projet_intra_Entreprise.Code.Classes.classe_Specialite import Specialite
+
+employe1 = Caissier()
 
 
 @pytest.mark.parametrize("poste, specialite, resultat_attendu", [
     (Caissier, "Viande", "Caissier Viande")
 ])
 def test_obtenir_poste_complet(poste, specialite, resultat_attendu):
-    employe1 = poste(p_specialite=Specialite(specialite))
+    specialite = Specialite(p_nom=specialite)
+    employe1.specialite =
     assert employe1.obtenir_poste_complet() == resultat_attendu
