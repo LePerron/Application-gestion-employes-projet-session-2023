a76afd16b71b846462d66fbb58dcf260fd3f552d
Maël <2256853@cegepoutaouais.qc.ca>
2024-05-06 09:54:01 -0400

    update

--- Code/Interfaces/Dialog/Dialog_Ajouter_Specialite.py
+++ Code/Interfaces/Dialog/Dialog_Ajouter_Specialite.py
@@ -1,3 +1,4 @@
+from Projet_intra_Entreprise.Code.Classes.classe_Specialite import Specialite
 from Projet_intra_Entreprise.Code.Interfaces.Code_Genere import genere_creer_specialite
 from PyQt5.QtCore import pyqtSlot
 from PyQt5 import QtWidgets
@@ -28,9 +29,18 @@ class AjouterSpecialite(QtWidgets.QDialog, genere_creer_specialite.Ui_DialogCree
 
     @pyqtSlot()
     def on_pushButtonAjouterSpecialite_clicked(self):
-        fenetre_ajouter_specialite = AjouterSpecialite()
-        fenetre_ajouter_specialite.show()
-        fenetre_ajouter_specialite.exec()
+        nom = self.lineEditNom.text().capitalize()
+        description = self.textEditDescription.toPlainText()
+
+        specialite_temporaire = Specialite()
+        specialite_temporaire.nom = nom
+
+        if specialite_temporaire.nom != nom:
+            self.labelErreurNom.setText("Nom incorrect")
+            del specialite_temporaire
+        else:
+            specialite_temporaire.description = description
+            self.close()
 
 
 def main():
