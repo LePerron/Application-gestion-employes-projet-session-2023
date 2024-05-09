02009730c8f6f504afa8d7eaa43755a46a70c1a0
benjamin <2371875@cegepoutaouais.qc.ca>
2024-05-09 09:36:03 -0400

    changement important

--- Code/Interfaces/Dialog/Dialog_MenuEmploye.py
+++ Code/Interfaces/Dialog/Dialog_MenuEmploye.py
@@ -37,7 +37,7 @@ class MenuEmploye(QtWidgets.QDialog, genere_menu_employe.Ui_DialogMenuEmploye):
         self.checkBoxAnciennete.stateChanged.connect(self.anciennete_checkbox_change)
         self.checkBoxContrat.stateChanged.connect(self.mettre_a_jour_listview)
         self.checkBoxDateEngagement.stateChanged.connect(self.mettre_a_jour_listview)
-        self.comboBoxTrierEmploye.currentIndexChanged.connect(self.mettre_a_jour_listview(self.comboBoxTrierEmploye.currentIndex()))
+        self.comboBoxTrierEmploye.currentIndexChanged.connect(self.mettre_a_jour_listview, self.comboBoxTrierEmploye.currentIndex())
         self.mettre_a_jour_listview()
 
     def mettre_a_jour_listview(self, current_index=None):
@@ -152,15 +152,15 @@ class MenuEmploye(QtWidgets.QDialog, genere_menu_employe.Ui_DialogMenuEmploye):
             self.comboBoxTrierEmploye.findText(self.comboBoxTrierEmploye.removeItem(self.comboBoxTrierEmploye.findText("Croissant (anciennté)")))
             self.comboBoxTrierEmploye.findText(self.comboBoxTrierEmploye.removeItem(self.comboBoxTrierEmploye.findText("Décroissant (anciennté)")))
 
-    def trier_a_z_list_view(self):
-        ifn
-    def trier_z_a_list_view(self):
-    def trier_croissant_salaire_list_view(self):
-    def trier_decroissant_salaire_list_view(self):
-    def trier_croissant_anciennete_list_view(self):
-
-    def trier_decroissant_anciennete_list_view(self):
-
+    # def trier_a_z_list_view(self):
+    #     ifn
+    # def trier_z_a_list_view(self):
+    # def trier_croissant_salaire_list_view(self):
+    # def trier_decroissant_salaire_list_view(self):
+    # def trier_croissant_anciennete_list_view(self):
+#
+    # def trier_decroissant_anciennete_list_view(self):
+#
 def main():
     """
     Méthode main : Point d'entré du programme.
